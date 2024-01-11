717966c714f50c698d65463daef761a95f6e312c
Bruno Lima <brunolimadevelopment@gmail.com>
2024-01-11 14:49:43 -0300

    feat: create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { BackgroundBox } from '../styles/loginScreen.styles';
+
+export default function LoginScreen() {
+  return (
+    <div>
+      <BackgroundBox src="./logo.svg" />
+    </div>
+  );
+}
---
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BackgroundBox = styled.img`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 367px;
+  height: 625px;
+  background-color: black;
+`;
