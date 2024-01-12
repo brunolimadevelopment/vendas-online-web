c32640f420ea7556be768387e550a335b5ced812
Bruno Lima <brunolimadevelopment@gmail.com>
2024-01-12 15:07:49 -0300

    feat: insert state in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,4 +1,5 @@
 import { MailOutlined, UserOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 import Button from '../../../shared/buttons/button/Button';
 import Input from '../../../shared/inputs/input/input';
@@ -12,6 +13,21 @@ import {
 } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    alert(`username: ${username}, password: ${password}`);
+  };
+
   return (
     <ContainerLogin>
       <BlackBox>
@@ -30,15 +46,20 @@ const LoginScreen = () => {
           placeholder="Digite seu nome"
           margin="15px 0 0"
           addonBefore={<UserOutlined />}
+          onChange={handleUsername}
+          value={username}
         />
         <Input
+          type="password"
           size="large"
-          title="Email"
-          placeholder="Digite seu email"
+          title="Password"
+          placeholder="Digite sua senha"
           margin="20px 0 0"
           addonBefore={<MailOutlined />}
+          onChange={handlePassword}
+          value={password}
         />
-        <Button size="large" type="primary" margin="20px 0 0">
+        <Button size="large" type="primary" margin="20px 0 0" onClick={handleLogin}>
           ENTRAR
         </Button>
       </WhiteBox>
