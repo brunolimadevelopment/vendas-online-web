d249ce9a0cb489f96aac8c5585972022449ad473
Bruno Lima <brunolimadevelopment@gmail.com>
2024-01-30 18:37:15 -0300

    feat: insert product in backend

---
+++ src/shared/components/inputs/select/select.styles.ts
@@ -0,0 +1,10 @@
+import { Typography } from 'antd';
+import styled from 'styled-components';
+
+const { Text } = Typography;
+
+export const BoxSelect = styled.div``;
+
+export const TitleSelect = styled(Text)`
+  color: #000;
+`;
